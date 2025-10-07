ec0710618b145685a7154ffada752c9b491d7f69
Jakub Tkacz <jtkacz@expo.io>
2025-10-07 12:12:28 +0200

    [expo-router][sdk-54] Web modal disable flag PoC

--- packages/expo-router/build/layouts/Stack.web.js
+++ packages/expo-router/build/layouts/Stack.web.js
@@ -2,16 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Stack = void 0;
 const StackClient_1 = require("./StackClient");
-const ModalStack_1 = require("../modal/web/ModalStack");
 const Protected_1 = require("../views/Protected");
-// The RouterModal already includes Screen and Protected via withLayoutContext
-// but we need to ensure we forward the stackRouterOverride for singular routes etc.
-const Stack = Object.assign((props) => {
-    return <ModalStack_1.RouterModal {...props} UNSTABLE_router={StackClient_1.stackRouterOverride}/>;
-}, {
-    Screen: ModalStack_1.RouterModalScreen,
-    Protected: Protected_1.Protected,
-});
+const Stack = (() => {
+    if (!process.env.EXPO_PUBLIC_DISABLE_NEW_WEB_MODAL) {
+        const { RouterModal, RouterModalScreen } = require('../modal/web/ModalStack');
+        // The RouterModal already includes Screen and Protected via withLayoutContext
+        // but we need to ensure we forward the stackRouterOverride for singular routes etc.
+        return Object.assign((props) => {
+            return <RouterModal {...props} UNSTABLE_router={StackClient_1.stackRouterOverride}/>;
+        }, {
+            Screen: RouterModalScreen,
+            Protected: Protected_1.Protected,
+        });
+    }
+    const Stack = require('./StackClient').default;
+    const Screen = require('../views/Screen').Screen;
+    Stack.Screen = Screen;
+    return Stack;
+})();
 exports.Stack = Stack;
 exports.default = Stack;
 //# sourceMappingURL=Stack.web.js.map
--- packages/expo-router/src/layouts/Stack.web.tsx
+++ packages/expo-router/src/layouts/Stack.web.tsx
@@ -1,21 +1,28 @@
 import { ComponentProps } from 'react';
 
 import { stackRouterOverride } from './StackClient';
-import { RouterModal, RouterModalScreen } from '../modal/web/ModalStack';
 import { Protected } from '../views/Protected';
 
-// The RouterModal already includes Screen and Protected via withLayoutContext
-// but we need to ensure we forward the stackRouterOverride for singular routes etc.
-
-const Stack = Object.assign(
-  (props: ComponentProps<typeof RouterModal>) => {
-    return <RouterModal {...props} UNSTABLE_router={stackRouterOverride} />;
-  },
-  {
-    Screen: RouterModalScreen,
-    Protected,
+const Stack = (() => {
+  if (!process.env.EXPO_PUBLIC_DISABLE_NEW_WEB_MODAL) {
+    const { RouterModal, RouterModalScreen } = require('../modal/web/ModalStack');
+    // The RouterModal already includes Screen and Protected via withLayoutContext
+    // but we need to ensure we forward the stackRouterOverride for singular routes etc.
+    return Object.assign(
+      (props: ComponentProps<typeof RouterModal>) => {
+        return <RouterModal {...props} UNSTABLE_router={stackRouterOverride} />;
+      },
+      {
+        Screen: RouterModalScreen,
+        Protected,
+      }
+    );
   }
-);
+  const Stack = require('./StackClient').default;
+  const Screen = require('../views/Screen').Screen;
+  Stack.Screen = Screen;
+  return Stack;
+})();
 
 export { Stack };
 
